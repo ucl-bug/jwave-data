8cd8299dd8181a4b7c080a2f1193da4d5bf7efde
Antonio Stanziola <stanziola.antonio@gmail.com>
2022-05-13 18:52:36 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## jwave test data
+
+To use, add a `SETTINGS` file with the path to your local jwave repository
+
+```
+JWAVE_PATH="/home/user/jwave/"
+```
+
+---
+
+Last updated: Fri 13 May 18:47:55 BST 2022
---
+++ get_data.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Loads the test data from the jwave repository to the appropriate folders
+# Usage: ./get_data.sh
+
+# Check if a file called SETTINGS exists, otherwise exit
+if [ ! -f SETTINGS ]; then
+  echo "SETTINGS not found. Exiting."
+  exit 1
+fi
+
+source SETTINGS
+
+echo "jwave location is: $JWAVE_PATH"
+echo "Syncing data..."
+
+# Perform rsync on $JWAVE_PATH/tests/kwave_data to the kwave_data folder
+rsync -av $JWAVE_PATH/tests/kwave_data/*.mat tests/kwave_data/
+
+echo "Data synced."
+
+# Remove last line of README.md
+sed -i '$ d' README.md
+
+# Write the current date to the README.md file
+echo "Last updated: $(date)" >> README.md
